d4a53f13472abff21e95f533c9a403db39b21802
ckleemann <ckleemann@users.noreply.github.com>
2019-12-25 20:38:45 +0100

    remove default file mode

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 
-with open("README.md", "r") as fh:
+with open('README.md') as fh:
     long_description = fh.read()
 
 with open('requirements.txt') as f:
