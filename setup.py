c4b032f84481e5b5f8de506cb4d888526b3df439
Heiko Carrasco <heiko.carrasco@yahoo.com>
2019-12-27 12:43:05 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='django-allauth-d120',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     license='AGPL',
