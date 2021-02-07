251211da0714e3fa2c5f53f21a3475913e044034
ckleemann <ckleemann@users.noreply.github.com>
2021-02-07 21:38:34 +0100

    bump to v0.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='django-allauth-d120',
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(),
     include_package_data=True,
     license='AGPL',
