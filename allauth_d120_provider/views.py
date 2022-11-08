7aaf32608050f1cfc7fc61d062b3f063718b5b43
Fabian Damken <fabian.damken@fachschaft.informatik.tu-darmstadt.de>
2022-11-08 16:52:19 +0100

    Update views.py

--- allauth_d120_provider/views.py
+++ allauth_d120_provider/views.py
@@ -27,7 +27,7 @@ class D120Adapter(OAuth2Adapter):
     authorize_url = provider_base(endpoint='auth')
 
     def complete_login(self, request, app, token, **kwargs):
-        headers = {'Authorization': 'Bearer {0}'.format(token.token)}
+        headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer {0}'.format(str(token))}
         resp = requests.get(self.profile_url, headers=headers)
         extra_data = resp.json()
         return self.get_provider().sociallogin_from_response(request, extra_data)
